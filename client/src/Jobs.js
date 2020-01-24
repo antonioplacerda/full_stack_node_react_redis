965b87bd033006ea1bdfbcae534f6ad3cd675344
Antonio P Lacerda <antonioplacerda@gmail.com>
2020-01-24 00:31:32 +0000

    create Jobs component to list jobs

---
+++ client/src/Jobs.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+export default function Jobs() {
+    return (
+        <Typography variant="h1">
+            Jobs
+        </Typography>
+    )
+}
