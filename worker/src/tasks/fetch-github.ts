a42b2688712bb5fe6903c04d99143b2f7f581ff0
Antonio P Lacerda <antonioplacerda@gmail.com>
2020-01-27 23:27:18 +0000

    filter results - only jr jobs

--- worker/src/tasks/fetch-github.ts
+++ worker/src/tasks/fetch-github.ts
@@ -1,25 +1,47 @@
 import fetch from 'node-fetch';
+import { createClient } from 'redis';
+const { promisify } = require('util');
 
 const baseURL = 'https://jobs.github.com/positions.json';
 
-async function fetchGithub() {
+const client = createClient();
+const setAsync = promisify(client.set).bind(client);
+
+export default async function fetchGithub() {
     const allJobs = [];
 
     let resultCount = 1;
     let onPage = 0;
 
-    while(onPage < 6) {
+    // fetch all pages
+    while (onPage < 6) {
         const res = await fetch(baseURL);
         const jobs = await res.json();
         allJobs.push(...jobs);
         resultCount = jobs.length;
         console.log('got', resultCount, 'jobs');
-        console.log('got', allJobs.length, 'jobs total');
         onPage++;
     }
     console.log('got', allJobs.length, 'jobs total');
+
+    // filter jobs
+    const jrJobs = allJobs.filter(job => {
+        const jobTitle = job.title.toLowerCase();
+
+        if (
+            jobTitle.includes('senior') ||
+            jobTitle.includes('manager') ||
+            jobTitle.includes('sr.') ||
+            jobTitle.includes('architect')
+        ) {
+            return false;
+        }
+
+        return true;
+    });
+
+    console.log('filtered down to ', jrJobs.length);
+
+    const success = await setAsync('github', JSON.stringify(allJobs));
+    console.log({success});
 }
-
-module.exports = fetchGithub;
-
-fetchGithub();
